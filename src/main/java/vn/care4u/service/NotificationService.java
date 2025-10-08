9459b7e8b6787c6371b0b993d7a79400d6e97939
Xuan Bao <work.xuanbao@gmail.com>
2025-10-09 00:02:11 +0700

    add notification function

--- src/main/java/vn/care4u/service/NotificationService.java
+++ src/main/java/vn/care4u/service/NotificationService.java
@@ -2,12 +2,23 @@ package vn.care4u.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+
+import vn.care4u.model.request.NotificationRequest;
 import vn.care4u.model.response.NotificationResponse;
 
 public interface NotificationService {
 
-	List<NotificationResponse> getAllNotificationsOfUser(String email);
+//	List<NotificationResponse> getAllNotificationsOfUser(String email);
 
 	NotificationResponse getNotificationById(Long id);
+
+	Page<NotificationResponse> getAllNotifications(String email, int page, int size);
+
+	List<NotificationResponse> getRecentNotifications(String email);
+
+	void markAsRead(Long id, String email);
+
+	void sendNotification(NotificationRequest req, String senderEmail);
 
 }
