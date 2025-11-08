fff752e9ceb7ad2d447ff233b01fa633c0e2b6ad
Ngọc Vân <nguyenngocvan.qng@gmail.com>
2025-11-08 11:53:45 +0700

    create appointment

--- src/main/java/vn/care4u/service/impl/DoctorServiceImpl.java
+++ src/main/java/vn/care4u/service/impl/DoctorServiceImpl.java
@@ -1,7 +1,6 @@
 package vn.care4u.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import vn.care4u.entity.Doctor;
